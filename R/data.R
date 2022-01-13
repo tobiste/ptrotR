c05c50864c684448cf6797391d27cad50e55ca8d
Tobias Stephan <tobias.stephan@ucalgary.ca>
2022-01-12 21:15:43 -0700

    added example datasets

---
+++ R/data.R
@@ -0,0 +1,27 @@
+#' @title Example rotation file
+#' @description Sequence of total reconstruction reotations in GPLATE's .rot format
+#'
+#' @docType data
+#'
+#' @usage data(pangea)
+#'
+#' @format An object of class \code{"finite"}
+#' @references <div class="csl-entry">Kroner, U., Roscher, M., &#38; Romer, R. L. (2016). Ancient plate kinematics derived from the deformation pattern of continental crust: Paleo- and Neo-Tethys opening coeval with prolonged Gondwana–Laurussia convergence. <i>Tectonophysics</i>, <i>681</i>, 220–233. https://doi.org/10.1016/j.tecto.2016.03.034</div>
+#' @examples
+#' data(pangea)
+#' head(pangea)
+'pangea'
+
+#' @title Example rotation file
+#' @description Sequence of total reconstruction reotations in GPLATE's .rot format
+#'
+#' @docType data
+#'
+#' @usage data(pannotia)
+#'
+#' @format An object of class \code{"finite"}
+#' @references <div class="csl-entry">Kroner, U., Stephan, T., Romer, R. L., &#38; Roscher, M. (2020). Paleozoic plate kinematics during the Pannotia–Pangaea supercontinent cycle. <i>Geological Society, London, Special Publications</i>, <i>503</i>, SP503-2020–15. https://doi.org/10.1144/SP503-2020-15</div>
+#' @examples
+#' data(pannotia)
+#' head(pannotia)
+'pannotia'
